d91203c9f3c88cedf10b5149a3dcb6d153fb0113
hwm <771734186@qq.com>
2019-02-14 15:21:25 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
+import App from './App'
+import router from './router'
+import store from './store'
 
 // 百度地图 https://dafrok.github.io/vue-baidu-map/#/
 import BaiduMap from 'vue-baidu-map'
