b9e4dfc2a74c3baa7db4538bb4126946c8d395a1
hwm09110 <771734186@qq.com>
2019-02-28 21:16:25 +0800

    update swiper autoplay

--- src/views/microwebsite/Home.vue
+++ src/views/microwebsite/Home.vue
@@ -2,8 +2,8 @@
   <div class="home-wrap">
     <van-tabs 
       v-model="tabActive"
-      line-width="20"
-      swipe-threshold="3.5"
+      :line-width="20"
+      :swipe-threshold="3.5"
       title-active-color="#3f3f3f"
       title-inactive-color="#8a9199" 
       @change="handleTabChange">
@@ -11,7 +11,7 @@
     </van-tabs>
     <!-- banner start -->
     <div class="banner-wrap">
-      <swiper :options="swiperOption" ref="mySwiper">
+      <swiper :options="swiperOption" ref="mySwiper" v-if="swiperSlides.length">
         <swiper-slide v-for="(slide, index) in swiperSlides" :key="index">
           <!-- <img src="../../assets/img/banner.png" alt="banner"> -->
           {{slide}}
@@ -42,12 +42,17 @@ export default {
       tabActive: 0, //type tab 选中的index
       categorys: ['校园新闻','文化园地','校园之星','校园活动','校园风采'],
       swiperOption: {
+        autoplay: {
+          disableOnInteraction: false,
+        },
+        // autoplay: true,
         loop: true,
-        slidesPerView: 'auto',
         loopedSlides: 1, //在loop模式下使用slidesPerview:'auto'，还需使用该参数设置所要用到的loop个数(一般设置为本来slide的个数)
+        slidesPerView: 'auto',
         centeredSlides: true,
+        disableOnInteraction: false, //设置为false，用户操作swiper之后自动切换不会停止，每次都会重新启动autoplay
         spaceBetween: 10,
-        initialSlide: 1 //初始化时active slide
+        initialSlide: 0 //初始化时active slide
       },
       swiperSlides: []
     }
@@ -64,20 +69,25 @@ export default {
       let length = Math.floor(Math.random() * 9) + 1
       var arr = Array.from({length}, (v,k) => k)
       console.log(arr)
-      this.swiperSlides = arr
-      this.swiperOption.loopedSlides = this.swiperSlides.length
+      //默认的是加载的空数组，等数组获取完再加载swiper组件，在swiper组件中增加一个v-if="swiperSlides.length" (关键)
+      this.swiperSlides = []
       setTimeout(() => {
-        this.swiper.update()
-      },200)
+        this.swiperSlides = arr
+        this.swiperOption.loopedSlides = this.swiperSlides.length
+        setTimeout(()=>{
+          this.swiper.update()
+        },100)
+      },500)
     }
   },
   created() {
   },
   mounted() {
     setTimeout(() => {
-      this.swiperSlides.push(1,2,3)
+      this.swiperSlides.push(1,2,3,4,5)
+      console.log(this.swiperSlides.length)
       this.swiperOption.loopedSlides = this.swiperSlides.length
-    },3000)
+    },2000)
   }
 }
 </script>
